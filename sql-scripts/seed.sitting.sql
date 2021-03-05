cfeffcf071ff2e7dbf939892f52cdfd9b7c98a4e
Brandon Christianson <b.christianson110@gmail.com>
2021-03-05 10:35:26 -0800

    3/5

---
+++ sql-scripts/seed.sitting.sql
@@ -0,0 +1,36 @@
+BEGIN;
+
+INSERT INTO sitting  (name, img1, img2, img3, notes)
+VALUES
+    ('Easy Pose', 'https://lh5.googleusercontent.com/DuhWFp7QBpR-X6whKehCvm233FCwqdy-5fjjm0JPlkq6euMSkPohWQbkPRO_-JNHyo-OBjSskRy_6-_hfgZNxLQaSyXFWKHepybaHAaN-yyAOmYUWq4Y2l3m0h5WvayGOL_D-Ina', 'https://www.huggermugger.com/wp-content/uploads/2018/01/sukhasana-meditation.jpg', 'http://arogyayogaschool.com/blog/wp-content/uploads/2017/04/Sukhasana-Help-Reduce-Your-Blood-Pressure.jpg', 'This is one way to do easy pose, but as the name suggests, this pose is intended to be a comfortable seat. If this configuration does not work for you, take any cross-legged position. Sitting on a blanket to elevate the hips is also encouraged here.'),
+    ('Thunderbolt Pose', 'https://i.pinimg.com/originals/78/8a/5d/788a5d0c9c852e1cdcd58a8c98fded8b.jpg', 'https://photos.cdn-outlet.com/yo-images/userfiles/guide/image/yoga/yoga_how-to-do-thunderbolt-pose_300x350_jpg.jpg', 'https://i.ytimg.com/vi/3ILEYJOtHgM/maxresdefault.jpg', 'A basic kneeling position, with the seat resting on the feet.'),
+    ('Staff Pose', 'https://www.verywellfit.com/thmb/u6DHrDtCYh8JD5-Kh7fQA-EGLcE=/1500x1000/filters:no_upscale():max_bytes(150000):strip_icc()/StaffPose_annotated-007b1534c2914848af88f7e09360c0b4.jpg', 'https://www.ekhartyoga.com/media/images/articles/content/Staff-Pose-Dandasana-Ekhart-Yoga.jpg', 'https://cdn.shopify.com/s/files/1/0032/7539/1011/articles/Yoga_SeatedStaffPose_01_300x350_1_600x.jpg?v=1558424546', 'Staff pose is often called the seated equivalent of mountain pose. Just as mountain sets up the alignment for many standing poses, staff pose is the starting point for many seated poses.'),
+    ('Seated Forward Bend', 'https://www.ekhartyoga.com/media/images/articles/content/Paschimottanasana-Seated-Forward-bend-Ekhart-Yoga.jpg', 'https://i.ytimg.com/vi/Xn1wigQSrmI/maxresdefault.jpg', 'https://cdn.drweil.com/wp-content/uploads/2016/12/health-wellness_balanced-living_exercise-fitness_seated-forward-bend-pose_2716x1811_000026479707-600x450.jpg', 'There are several approaches to coming into seated forward bend, but try to keep the back flat as you tip the pelvis forward.');
+    ('Seated Wide Legged Straddle', 'https://i.ytimg.com/vi/GVo52kzskAg/maxresdefault.jpg', 'https://awakenwellness.files.wordpress.com/2012/06/seatedwideleg.jpg', 'https://www.yogateket.com/image/original/Seated-Wide-Angle-Straddle.jpg', 'You do not have to go for a super wide angle with the legs. Feeling some stretch in the inner thigh is common, but you do not need to go for a full split.');
+    ('Cobbler Pose', 'https://www.verywellfit.com/thmb/nBgqjt8A7V3uEceRK2hsiGCiDeg=/1500x1000/filters:no_upscale():max_bytes(150000):strip_icc()/CobblersPose_annotated-b6b590ebb22c4da1948cbd78ff1651d1.jpg', 'https://i.ytimg.com/vi/s5265rtPTMg/maxresdefault.jpg', 'https://asmy.org.au/app/uploads/2016/12/Cobbler-795x455.jpg', 'Cobbler pose can be taken into a forward bend if your groin allows it. Light pressure with your forearms on your legs can intensify the stretch.'),
+    ('Half Lord of the Fishes Pose', 'https://www.verywellfit.com/thmb/tVqnwWu8IjsKJNe9CzypK9SYJsE=/1500x1000/filters:no_upscale():max_bytes(150000):strip_icc()/HalfLordOfTheFishes_annotated-d4bb4f3d70f444a5966d2018bbee8564.jpg', 'https://www.ekhartyoga.com/media/image/articles/Ardha-Matsyendrasana-Half-lord-of-the-fishes-yoga-pose.jpg', 'https://www.gaia.com/wp-content/uploads/Meta-Half-Lord-of-fish-1024X576-768x432.jpg', 'Half lord of the fishes is also known as a seated spinal twist. It is important to keep your front foot and sit bones grounded to the floor here so that you have something to anchor the twist. Switch after 30 seconds.'),
+    ('Cow Face Pose', 'https://www.verywellfit.com/thmb/syR23KHuv2Tw9apgCyrwuTn-86g=/1500x1000/filters:no_upscale():max_bytes(150000):strip_icc()/CowFacePoseGomukhasana_annotated-49620d2c06104d369a1c5617054500b5.jpg', 'https://i.pinimg.com/originals/83/1f/ea/831fea8661e48fc513da23aaaa7bba53.jpg', 'https://www.ekhartyoga.com/media/images/articles/content/Cow-face-pose-Gomukhasana-Esther-Ekhart-292x292.jpg', 'Cow face pose gets into parts of the shoulders and upper arms that are rarely used by many of us. Do not be surprised to discover that it is much easier to bind the hands on one side than the other. This kind of asymmetry is very common, since most people favor one hand over the other. Switch after 30 seconds.'),
+    ();
+    ();
+    (),
+    (),
+    (),
+    ();
+    ();
+    (),
+    (),
+    (),
+    ();
+    ();
+    (),
+    (),
+    (),
+    ();
+    ();
+    (),
+    (),
+    (),
+    ();
+    ();
+
+COMMIT;
