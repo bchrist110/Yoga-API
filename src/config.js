2a50e3db4adb5e39d235bf8de0a77f4844d9ebb7
bchrist110 <bchristianson110@gmail.com>
2024-10-12 14:17:19 -0500

    fixing backend

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://dunder_mifflin@localhost/yoga',
+    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres.nlfqqctapyuqrolznamt:Qd2Ka!Q3B*.K$Bq@aws-0-us-west-1.pooler.supabase.com:6543/postgres',
 }
